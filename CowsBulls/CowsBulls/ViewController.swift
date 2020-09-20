629003fe0b937e4b89e70b334301e6eaf638fd5f
Salvador Rodriguez <salvador.rodriguez01@sjsu.edu>
2020-09-19 19:50:05 -0700

    inital project

---
+++ CowsBulls/CowsBulls/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CowsBulls
+//
+//  Created by Salvador Rodriguez on 9/19/20.
+//  Copyright © 2020 Salvador Rodriguez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
